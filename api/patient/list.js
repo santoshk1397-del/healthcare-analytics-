970f96d82e3902bd9d4519be87724379e279080c
santoshk1397-del <santosh.k1397@gmail.com>
2026-04-15 22:29:51 +0530

    Create list.js

---
+++ api/patient/list.js
@@ -0,0 +1,6 @@
+import { supabase } from "../db";
+
+export default async function handler(req, res) {
+  const { data } = await supabase.from("patients").select("*");
+  res.status(200).json({ patients: data });
+}
