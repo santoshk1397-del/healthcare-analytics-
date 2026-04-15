80edda451fcd647fa36a7c3e8d8af3482b78220e
santoshk1397-del <santosh.k1397@gmail.com>
2026-04-16 00:48:34 +0530

    Update create.js

--- api/screening/create.js
+++ api/screening/create.js
@@ -1,4 +1,4 @@
-import { supabase } from "../db";
+import { supabase } from "../db.js";
 
 export default async function handler(req, res) {
   if (req.method !== "POST") return res.status(405).end();
